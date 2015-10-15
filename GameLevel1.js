50f7d39e7e1ec3c4cf91372290cd8a4f6a6bef8b
JaydenReed <JaydenReed@outlook.com>
2015-10-15 21:28:39 +1100

    More Level Collisions and Player Movement
    Added more walls and a checklist to see which walls are currently done.

--- GameLevel1.js
+++ GameLevel1.js
@@ -32,25 +32,84 @@ function level1(deltaTime)
 		width: 116,
 		height: 7
 	}
-	var boundary4 = {
-		x: 612,
-		y: 0,
-		width: 328,
-		height: 270
-	}
-	var boundary5 = {
-		x: 499,
-		y: 510,
-		width: 441,
-		height: 190
-	}
-	var boundary6 = {
-		x: 720,
+	var wall4 = {
+		x: 721,
 		y: 264,
-		width: 220,
+		width: 7,
 		height: 252
 	}
-
+	var wall5 = {
+		x: 504,
+		y: 510,
+		width: 223,
+		height: 6
+	}
+	var wall6 = {
+		x: 499,
+		y: 510,
+		width: 7,
+		height: 82
+	}
+	var wall7 = {
+		x: 258,
+		y: 586,
+		width: 248,
+		height: 6
+	}
+	var wall8 = {
+		x: 258,
+		y: 103,
+		width: 7,
+		height: 489
+	}
+	var innerWall1 = {
+		x: 392,
+		y: 108,
+		width: 4,
+		height: 29
+	}
+	var innerWall2 = {
+		x: 396,
+		y: 108,
+		width: 4,
+		height: 30
+	}
+	var innerWall3 = {
+		x: 392,
+		y: 170,
+		width: 3,
+		height: 224
+	}
+	var innerWall4 = {
+		x: 397,
+		y: 170,
+		width: 3,
+		height: 224
+	}
+	var innerWall5 = {
+		x: 266,
+		y: 393,
+		width: 46,
+		height: 3
+	}
+	var innerWall6 = {
+		x: 266,
+		y: 398,
+		width: 46,
+		height: 3
+	}
+	var innerWall7 = {
+		x: 347,
+		y: 394,
+		width: 47,
+		height: 2
+	}
+	var innerWall8 = {
+		x: 347,
+		y: 399,
+		width: 47,
+		height: 2
+	}
 	// Code to draw player
 	//player.update(deltaTime);
 	context.save();
@@ -58,7 +117,7 @@ function level1(deltaTime)
 		context.drawImage(player.image, player.width - player.width, player.height - player.height);
 	context.restore();
 	
-	playerMovement(wall1, wall2, wall3);
+	playerMovement(wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, innerWall1, innerWall2, innerWall3, innerWall4, innerWall5, innerWall6, innerWall7, innerWall8);
 
 	level1Doors()
 }
--- Player.js
+++ Player.js
@@ -28,7 +28,7 @@ var player = {
 };
 player.image.src = "ProtoPlayer.png";
 
-function playerMovement(wall1, wall2, wall3)
+function playerMovement(wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, innerWall1, innerWall2, innerWall3, innerWall4, innerWall5, innerWall6, innerWall7, innerWall8)
 {
 	var hit = intersects(player.x, player.y, player.width, player.height, wall1.x, wall1.y, wall1.width, wall1.height);
 	if(hit == true)
@@ -41,6 +41,71 @@ function playerMovement(wall1, wall2, wall3)
 		player.x -= 4;
 	}
 	var hit = intersects(player.x, player.y, player.width, player.height, wall3.x, wall3.y, wall3.width, wall3.height);
+	if(hit == true)
+	{
+		player.y += 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, wall4.x, wall4.y, wall4.width, wall4.height);
+	if(hit == true)
+	{
+		player.x -= 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, wall5.x, wall5.y, wall5.width, wall5.height);
+	if(hit == true)
+	{
+		player.y -= 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, wall6.x, wall6.y, wall6.width, wall6.height);
+	if(hit == true)
+	{
+		player.x -= 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, wall7.x, wall7.y, wall7.width, wall7.height);
+	if(hit == true)
+	{
+		player.y -= 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, wall8.x, wall8.y, wall8.width, wall8.height);
+	if(hit == true)
+	{
+		player.x += 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, innerWall1.x, innerWall1.y, innerWall1.width, innerWall1.height);
+	if(hit == true)
+	{
+		player.x -= 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, innerWall2.x, innerWall2.y, innerWall2.width, innerWall2.height);
+	if(hit == true)
+	{
+		player.x += 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, innerWall3.x, innerWall3.y, innerWall3.width, innerWall3.height);
+	if(hit == true)
+	{
+		player.x -= 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, innerWall4.x, innerWall4.y, innerWall4.width, innerWall4.height);
+	if(hit == true)
+	{
+		player.x += 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, innerWall5.x, innerWall5.y, innerWall5.width, innerWall5.height);
+	if(hit == true)
+	{
+		player.y -= 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, innerWall6.x, innerWall6.y, innerWall6.width, innerWall6.height);
+	if(hit == true)
+	{
+		player.y += 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, innerWall7.x, innerWall7.y, innerWall7.width, innerWall7.height);
+	if(hit == true)
+	{
+		player.y -= 4;
+	}
+	var hit = intersects(player.x, player.y, player.width, player.height, innerWall8.x, innerWall8.y, innerWall8.width, innerWall8.height);
 	if(hit == true)
 	{
 		player.y += 4;
