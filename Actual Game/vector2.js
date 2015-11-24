9c8a76abd724ad2e818e8e1ac81a7a1e391c247b
AusvLux <C.Lewer@live.com>
2015-11-24 18:10:55 +1100

    Update:
    added bullet.js bullet.png and vector2.js

---
+++ Actual Game/vector2.js
@@ -0,0 +1,41 @@
+var Vector2 = function() {
+	this.x;
+	this.y;
+}
+
+Vector2.prototype.set = function(x,y)
+{
+	this.x = x;
+	this.y = y;
+}
+
+Vector2.prototype.normalize = function()
+{
+	var length = Math.sqrt(this.x*this.x + this.y*this.y);
+	
+	var normalX = this.x / length;
+	var normalY = this.y / length;
+}
+
+Vector2.prototype.add = function(v2)
+{
+	this.x = this.x + v2;
+	this.y = this.y + v2;
+}
+
+Vector2.prototype.subtract = function(v2)
+{
+	this.x = this.x - v2;
+	this.y = this.y - v2;
+}
+
+Vector2.prototype.multiplyScalar = function(num)
+{
+	this.x = num * this.x;
+	this.y = num * this.y;
+}
+
+Vector2.prototype.Magnitude = function()
+{
+	return  Math.sqrt((this.x * this.x) + (this.y * this.y));
+};
