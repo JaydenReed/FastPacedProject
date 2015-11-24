a1fd357b49afc041ec3c54c2219a059d666831e3
Jayden <JaydenReed@outlook.com>
2015-11-24 18:37:04 +1100

    Door Javascript
    Added the door javascript and images for them

--- Actual Game/doors.js
+++ Actual Game/doors.js
@@ -1,3 +1,28 @@
+var level1Door = {
+	image: document.createElement("img"),
+	x: 448,
+	y: 250,
+	width: 8,
+	height: 60
+};
+level1Door.image.src = "DoorVertical.png";
+var level2Door = {
+	image: document.createElement("img"),
+	x: 501,
+	y: 379,
+	width: 60,
+	height: 8
+};
+level2Door.image.src = "DoorHorizontal.png";
+var level3Door = {
+	image: document.createElement("img"),
+	x: 774,
+	y: 379,
+	width: 60,
+	height: 8
+};
+level3Door.image.src = "DoorHorizontal.png";
+
 function doors()
 {
 	
